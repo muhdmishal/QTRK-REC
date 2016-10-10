6511787316c9aba6adc282c4b7a241373ee23197
Muhammed Mishal <Muhammed Mishal>
2016-10-10 11:24:35 +0530

    First Commit

---
+++ Qtracker/Qtracker/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Qtracker
+{
+    partial class login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.username = new System.Windows.Forms.TextBox();
+            this.message = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(27, 79);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(230, 20);
+            this.username.TabIndex = 0;
+            // 
+            // message
+            // 
+            this.message.AutoSize = true;
+            this.message.ForeColor = System.Drawing.Color.Red;
+            this.message.Location = new System.Drawing.Point(24, 37);
+            this.message.MaximumSize = new System.Drawing.Size(250, 100);
+            this.message.MinimumSize = new System.Drawing.Size(230, 30);
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(230, 30);
+            this.message.TabIndex = 1;
+            this.message.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.message.Click += new System.EventHandler(this.logo_Click);
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(27, 137);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(230, 20);
+            this.password.TabIndex = 2;
+            this.password.UseSystemPasswordChar = true;
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(27, 191);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(230, 23);
+            this.btn_login.TabIndex = 3;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.message);
+            this.Controls.Add(this.username);
+            this.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.Name = "login";
+            this.Text = "Qtracker Login";
+            this.Load += new System.EventHandler(this.login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.Label message;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Button btn_login;
+    }
+}
+
---
+++ Qtracker/Qtracker/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace Qtracker
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void logo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            message.Text = "";
+            try
+            {
+                string _url = "http://tecque.com/qtracker/login.php";
+                string _username = username.Text;
+                string _password = password.Text;
+
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(_url);
+                req.Method = "POST";
+                string Data = "username=" + _username + "&password=" + _password;
+                byte[] postBytes = Encoding.ASCII.GetBytes(Data);
+                req.ContentType = "application/x-www-form-urlencoded";
+                req.ContentLength = postBytes.Length;
+                Stream requestStream = req.GetRequestStream();
+                requestStream.Write(postBytes, 0, postBytes.Length);
+                requestStream.Close();
+
+                HttpWebResponse response = (HttpWebResponse)req.GetResponse();
+                Stream resStream = response.GetResponseStream();
+
+                var sr = new StreamReader(response.GetResponseStream());
+                string responseText = sr.ReadToEnd();
+                message.Text = responseText;
+            }
+            catch (WebException)
+            {
+                MessageBox.Show("Please Check Your Internet Connection");
+            }
+        }
+    }
+}
